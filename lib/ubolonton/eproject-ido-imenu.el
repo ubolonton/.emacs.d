ad1a37d5368b45676d27818a719c68a18d2cdad0
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2011-11-06 03:28:13 +0800

    add `ido-imenu' which was removed from emacs-starter-kit

--- lib/ubolonton/eproject-ido-imenu.el
+++ lib/ubolonton/eproject-ido-imenu.el
@@ -171,6 +171,50 @@ converted to a form that `eproject--ido-symbols' can use."
     (list (apply #'append (mapcar #'car index))
           (apply #'append (mapcar #'cadr index)))))
 
+;;; XXX: This was removed from emacs-starter-kit since in favor of
+;;; "omni-ido", but that thing has problem with imenu nesting menus
+(defun ido-imenu ()
+  "Update the imenu index and then use ido to select a symbol to navigate to.
+  Symbols matching the text at point are put first in the completion list."
+  (interactive)
+  (imenu--make-index-alist)
+  (let ((name-and-pos '())
+        (symbol-names '()))
+    (flet ((addsymbols (symbol-list)
+                       (when (listp symbol-list)
+                         (dolist (symbol symbol-list)
+                           (let ((name nil) (position nil))
+                             (cond
+                              ((and (listp symbol) (imenu--subalist-p symbol))
+                               (addsymbols symbol))
+
+                              ((listp symbol)
+                               (setq name (car symbol))
+                               (setq position (cdr symbol)))
+
+                              ((stringp symbol)
+                               (setq name symbol)
+                               (setq position (get-text-property 1 'org-imenu-marker symbol))))
+
+                             (unless (or (null position) (null name))
+                               (add-to-list 'symbol-names name)
+                               (add-to-list 'name-and-pos (cons name position))))))))
+      (addsymbols imenu--index-alist))
+    ;; If there are matching symbols at point, put them at the beginning of `symbol-names'.
+    (let ((symbol-at-point (thing-at-point 'symbol)))
+      (when symbol-at-point
+        (let* ((regexp (concat (regexp-quote symbol-at-point) "$"))
+               (matching-symbols (delq nil (mapcar (lambda (symbol)
+                                                     (if (string-match regexp symbol) symbol))
+                                                   symbol-names))))
+          (when matching-symbols
+            (sort matching-symbols (lambda (a b) (> (length a) (length b))))
+            (mapc (lambda (symbol) (setq symbol-names (cons symbol (delete symbol symbol-names))))
+                  matching-symbols)))))
+    (let* ((selected-symbol (ido-completing-read "Symbol? " symbol-names))
+           (position (cdr (assoc selected-symbol name-and-pos))))
+      (goto-char position))))
+
 (defun eproject-ido-imenu ()
   "Use ido to select imenu candidates (symbols) from opened
   buffers of the current project, as defined by eproject. Fall
@@ -216,4 +260,3 @@ Argument REGEXP-LIST is a list of regexps to combine."
   (format "\\(?:%s\\)"
           (reduce (lambda (a b) (concat a "\\|" b))
                   (mapcar (lambda (f) (format "\\(?:%s\\)" f)) regexp-list))))
-
