1b051d8a078ff1a1861f8381b039a40218072f56
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2013-10-07 14:26:22 +0700

    use ido for completing-read again

--- config/ublt-ido.el
+++ config/ublt-ido.el
@@ -13,4 +13,32 @@
 (ublt/set-up 'smex
   (setq smex-save-file "~/.emacs.d/.smex-items"))
 
+;; ido on steroid `http://www.emacswiki.org/emacs/InteractivelyDoThing'
+(defvar ido-enable-replace-completing-read t
+  "If t, use ido-completing-read instead of completing-read if possible.
+
+    Set it to nil using let in around-advice for functions where the
+    original completing-read is required.  For example, if a function
+    foo absolutely must use the original completing-read, define some
+    advice like this:
+
+    (defadvice foo (around original-completing-read-only activate)
+      (let (ido-enable-replace-completing-read) ad-do-it))")
+(defadvice completing-read
+  (around use-ido-when-possible activate)
+  (if (or (not ido-enable-replace-completing-read) ; Manual override disable ido
+          (and (boundp 'ido-cur-list) ido-cur-list)
+                                        ;ido-cur-list ; Emacs 24
+          )  ; Avoid infinite loop from ido calling this
+      ad-do-it
+    (let ((allcomp (all-completions "" collection predicate)))
+      (if allcomp
+          (setq ad-return-value
+                (ido-completing-read
+                 prompt
+                 allcomp
+                 nil require-match initial-input hist def))
+        ad-do-it))))
+
+
 (provide 'ublt-ido)
