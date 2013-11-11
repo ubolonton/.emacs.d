cdb49c08e2f1d02a300a927214cd932801878424
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2013-11-11 23:03:40 +0700

    add missing package ac-nrepl

--- init.el
+++ init.el
@@ -50,7 +50,7 @@
          flymake
          haskell-mode quack
          markdown-mode less-css-mode yaml-mode
-         clojure-mode clojurescript-mode durendal swank-clojure nrepl
+         clojure-mode clojurescript-mode durendal swank-clojure nrepl ac-nrepl
          elisp-slime-nav
          js2-mode flymake-jshint
          php-mode php-boris flymake-php
