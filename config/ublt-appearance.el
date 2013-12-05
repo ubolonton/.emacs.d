4831b370c4c2fc2a253f896a5792344f9ca5bfba
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2013-12-05 07:57:41 +0700

    add command to toggle between two gamma values: 2.7 & 5

--- config/ublt-appearance.el
+++ config/ublt-appearance.el
@@ -24,39 +24,47 @@
                  ;; And this, my new monitor, even after calibration?!?
                  (screen-gamma . 2.7)
                  ;; Daylight adaptation
-                 ;; (screen-gamma . 4)
-                 ))))
+                 ;; (screen-gamma . 5)
+                 ))
+              (defun ublt/toggle-gamma ()
+                (interactive)
+                (modify-all-frames-parameters
+                 `((screen-gamma . ,(let ((g (frame-parameter nil 'screen-gamma)))
+                                      (if (= g 2.7)
+                                          5 2.7))))))))
+
+
 
 
 ;;; Fonts
 (when window-system
   ;; Font-mixing obsession
   (ublt/set-up 'ublt-font
-  ;; Non-code text reads better in proportional font
-  (dolist (hook '(erc-mode-hook
-                  Info-mode-hook
-                  help-mode-hook
-                  apropos-mode-hook
-                  ess-help-mode-hook
-                  lyric-mode-hook
-                  Man-mode-hook woman-mode-hook
-                  twittering-mode-hook
-                  emms-playlist-mode-hook
-                  skype--chat-mode-hook
-                  org-mode-hook
-                  markdown-mode-hook
-                  html-mode-hook
-                  dired-mode-hook
-                  git-commit-mode-hook
-                  twittering-edit-mode-hook
-                  package-menu-mode-hook))
-    (add-hook hook (ublt/on-fn 'variable-pitch-mode)))
-  ;; This isn't pretty, but the alternative is using a variable-pitch
-  ;; font as the default, and creating a `fixed-pitch-mode'.
-  (defun ublt/variable-pitch-if-fundamental ()
-    (when (eq major-mode 'fundamental-mode)
-      (variable-pitch-mode +1)))
-  (add-hook 'find-file-hook 'ublt/variable-pitch-if-fundamental)))
+    ;; Non-code text reads better in proportional font
+    (dolist (hook '(erc-mode-hook
+                    Info-mode-hook
+                    help-mode-hook
+                    apropos-mode-hook
+                    ess-help-mode-hook
+                    lyric-mode-hook
+                    Man-mode-hook woman-mode-hook
+                    twittering-mode-hook
+                    emms-playlist-mode-hook
+                    skype--chat-mode-hook
+                    org-mode-hook
+                    markdown-mode-hook
+                    html-mode-hook
+                    dired-mode-hook
+                    git-commit-mode-hook
+                    twittering-edit-mode-hook
+                    package-menu-mode-hook))
+      (add-hook hook (ublt/on-fn 'variable-pitch-mode)))
+    ;; This isn't pretty, but the alternative is using a variable-pitch
+    ;; font as the default, and creating a `fixed-pitch-mode'.
+    (defun ublt/variable-pitch-if-fundamental ()
+      (when (eq major-mode 'fundamental-mode)
+        (variable-pitch-mode +1)))
+    (add-hook 'find-file-hook 'ublt/variable-pitch-if-fundamental)))
 
 
 ;;; Ubolonton's theme
