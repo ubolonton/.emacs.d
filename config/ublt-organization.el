5997e6a2835cfd4a3e385230c223ae396cc74036
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2013-12-15 12:40:35 +0700

    update organization stuff (org-agenda is currently unused)

--- config/ublt-organization.el
+++ config/ublt-organization.el
@@ -1,5 +1,7 @@
 (require 'ublt-util)
 
+(require 'org)
+
 (ublt/set-up 'org-indent
   (add-hook 'org-mode-hook (ublt/on-fn 'org-indent-mode)))
 
@@ -10,6 +12,10 @@
 
  ;; Show all headlines by default
  org-startup-folded 'content
+
+ org-catch-invisible-edits 'smart
+
+ ;; org-show-entry-below t
 
  ;; Don't use isearch there, normal isearch is enough
  org-goto-auto-isearch nil
@@ -41,7 +47,46 @@
                                  ("1)" . "1."))
 
  ;; org-footnote-auto-adjust t
+
+ ;; Fontify code blocks
+ org-src-fontify-natively t
+
+ org-export-htmlize-output-type 'css
  )
+
+(setq
+ ;; TODO: Maybe just file-local
+ org-todo-keywords
+ '((sequence "TODO(t)" "STARTED(s)" "|" "DONE(d)")
+   (sequence "THINK" "WRITE(w)" "|" "PUBLISHED(p)")
+   (sequence "DESIGN" "IMPLEMENT(i)" "TEST" "|" "DONE(d)")
+   (sequence "|" "CANCELED(c)"))
+
+ ;; Use with `org-toggle-ordered-property'
+ org-enforce-todo-dependencies t
+
+ ;; Log a lot
+ org-log-done 'note
+ org-log-reschedule 'note
+ org-log-repeat 'note
+ org-log-redeadline t
+ org-log-note-clock-out t)
+
+(ublt/set-up 'org-agenda
+  (setq
+   org-agenda-dim-blocked-tasks t
+   org-agenda-skip-scheduled-if-deadline-is-shown t))
+
+(ublt/set-up 'org-clock
+  (setq
+   org-clock-persist 'history)
+  (org-clock-persistence-insinuate))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (clojure . t)
+   (sh . t)))
 
 
 (add-to-list 'auto-mode-alist '("\\.\\(org\\)$" . org-mode))
@@ -54,65 +99,65 @@
       org-agenda-skip-deadline-if-done t
       org-agenda-skip-scheduled-if-done t
       org-agenda-span 7)
-(setq
- org-agenda-files '("~/gtd/gtd.org"
-                    "~/gtd/journal.org"
-                    "~/gtd/projects.org")
- org-default-notes-file "~/gtd/someday.org"
- org-columns-default-format "%TODO %50ITEM %TAGS %CATEGORY"
- org-stuck-projects '("+PROJECT/-DONE" ("DONE") ("*") "")
- org-todo-keywords '((sequence "TODO(t)" "STARTED(s!)" "|" "DONE(d!/!)"))
- org-todo-keyword-faces
- '(("TODO" :foreground "Red" :weight normal)
-   ("DEFERRED" :foreground "DeepSkyBlue" :weight normal)
-   ("STARTED" :foreground "DarkGoldenRod" :weight normal)
-   ("CANCELLED" :foreground "Gray15" :weight normal)
-   ("DONE" :foreground "LightGreen" :weight normal)
-   )
- org-refile-targets '((("~/gtd/projects.org") . (:maxlevel . 2))
-                      (("~/gtd/gtd.org") . (:maxlevel . 1))
-                      (("~/gtd/someday.org") . (:maxlevel . 1)))
- org-remember-templates
- '(("Todo"    ?t "** TODO %^{Brief Description} %^g\n   - Added: %U\n%?"   "~/gtd/gtd.org" "Tasks")
-   ("Someday" ?s "** TODO %^{Do this someday} %^g\n   - Added: %U\n%?"     "~/gtd/someday.org")
-   ("Diary"   ?d "** %U %^{Diary} :Diary:%^g\n%i%?"                        "~/gtd/journal.org")
-   ("Idea"    ?i "** %U %^{Thought} :Thought:%^g\n%i%?"                    "~/gtd/journal.org")
-   ("Review"  ?r "** %t Daily Review :Coach:\n%[~/gtd/daily-review.txt]\n" "~/gtd/journal.org")
-   ("Project" ?p "** TODO %^{Project Name}\n  - Added: %U\n%?"             "~/gtd/projects.org" "Projects"))
- org-agenda-custom-commands
- '(("P" "Projects" ((tags "PROJECT")))
-   ("D" "Daily action list"
-    ((agenda "" ((org-agenda-ndays 1)
-                 (org-agenda-sorting-strategy
-                  '((agenda time-up priority-down tag-up)))
-                 (org-deadline-warning-days 0)))))
-   ("S" "'Someday' task list"
-    ((todo "TODO" ((org-agenda-files '("~/gtd/someday.org"))
-                   (org-agenda-prefix-format '((todo . "  ")))))))))
-(setq org-src-fontify-natively t)
-(setq org-babel-scheme-cmd "mzscheme")
-(add-hook 'org-agenda-mode-hook (ublt/on-fn 'hl-line-mode))
+
+;; (setq
+;;  org-agenda-files '("~/gtd/gtd.org"
+;;                     "~/gtd/journal.org"
+;;                     "~/gtd/projects.org")
+;;  org-default-notes-file "~/gtd/someday.org"
+;;  org-columns-default-format "%TODO %50ITEM %TAGS %CATEGORY"
+;;  org-stuck-projects '("+PROJECT/-DONE" ("DONE") ("*") "")
+;;  org-todo-keywords '((sequence "TODO(t)" "STARTED(s!)" "|" "DONE(d!/!)"))
+;;  org-todo-keyword-faces
+;;  '(("TODO" :foreground "Red" :weight normal)
+;;    ("DEFERRED" :foreground "DeepSkyBlue" :weight normal)
+;;    ("STARTED" :foreground "DarkGoldenRod" :weight normal)
+;;    ("CANCELLED" :foreground "Gray15" :weight normal)
+;;    ("DONE" :foreground "LightGreen" :weight normal)
+;;    )
+;;  org-refile-targets '((("~/gtd/projects.org") . (:maxlevel . 2))
+;;                       (("~/gtd/gtd.org") . (:maxlevel . 1))
+;;                       (("~/gtd/someday.org") . (:maxlevel . 1)))
+;;  org-remember-templates
+;;  '(("Todo"    ?t "** TODO %^{Brief Description} %^g\n   - Added: %U\n%?"   "~/gtd/gtd.org" "Tasks")
+;;    ("Someday" ?s "** TODO %^{Do this someday} %^g\n   - Added: %U\n%?"     "~/gtd/someday.org")
+;;    ("Diary"   ?d "** %U %^{Diary} :Diary:%^g\n%i%?"                        "~/gtd/journal.org")
+;;    ("Idea"    ?i "** %U %^{Thought} :Thought:%^g\n%i%?"                    "~/gtd/journal.org")
+;;    ("Review"  ?r "** %t Daily Review :Coach:\n%[~/gtd/daily-review.txt]\n" "~/gtd/journal.org")
+;;    ("Project" ?p "** TODO %^{Project Name}\n  - Added: %U\n%?"             "~/gtd/projects.org" "Projects"))
+;;  org-agenda-custom-commands
+;;  '(("P" "Projects" ((tags "PROJECT")))
+;;    ("D" "Daily action list"
+;;     ((agenda "" ((org-agenda-ndays 1)
+;;                  (org-agenda-sorting-strategy
+;;                   '((agenda time-up priority-down tag-up)))
+;;                  (org-deadline-warning-days 0)))))
+;;    ("S" "'Someday' task list"
+;;     ((todo "TODO" ((org-agenda-files '("~/gtd/someday.org"))
+;;                    (org-agenda-prefix-format '((todo . "  ")))))))))
+
+;; (setq org-babel-scheme-cmd "mzscheme")
+;; (add-hook 'org-agenda-mode-hook (ublt/on-fn 'hl-line-mode))
 ;; (add-hook 'org-agenda-mode-hook 'esk-local-column-number-mode)
 (add-hook 'org-mode-hook (ublt/on-fn 'hl-line-mode))
 ;; (add-hook 'org-mode-hook 'esk-local-column-number-mode)
-(require 'org-install)
-(require 'remember)
-(require 'org-remember)
-(org-remember-insinuate)
-(require 'ob-scheme)
+
+;; (require 'remember)
+;; (require 'org-remember)
+;; (org-remember-insinuate)
 
 ;; Use external browser
 (add-to-list 'org-file-apps '("\\.x?html?\\'" browse-url file))
 
-;;; Wordpress blogging
-(ublt/set-up 'org2blog-autoloads
-  (setq org2blog/wp-blog-alist '(("ubolonton"
-                                  :url "http://ubolonton.wordpress.com/xmlrpc.php"
-                                  :username "ubolonton"
-                                  :default-title "From Emacs"
-                                  :default-categories ("org2blog" "emacs")
-                                  :tags-as-categories nil))
-        org2blog/wp-confirm-post nil))
+;; ;;; Wordpress blogging
+;; (ublt/set-up 'org2blog-autoloads
+;;   (setq org2blog/wp-blog-alist '(("ubolonton"
+;;                                   :url "http://ubolonton.wordpress.com/xmlrpc.php"
+;;                                   :username "ubolonton"
+;;                                   :default-title "From Emacs"
+;;                                   :default-categories ("org2blog" "emacs")
+;;                                   :tags-as-categories nil))
+;;         org2blog/wp-confirm-post nil))
 
 (ublt/set-up 'org-html-slideshow
   (defun ublt/org-html-slideshow-decorate ()
@@ -139,19 +184,20 @@
 "))
       (call-interactively 'org-export-as-html))))
 
-(ublt/set-up 'o-blog)
-
-(setq org-publish-project-alist
-      '(("blog"
-         :base-directory "~/Programming/projects/blog/src/"
-         :publishing-directory "~/Programming/projects/blog/public")
-        ("o-blog"
-         :base-directory "/home/ubolonton/.emacs.d/lib/o-blog/example/"
-         :publishing-directory "/home/ubolonton/.emacs.d/lib/o-blog/example/out"))
-      org-export-htmlize-output-type 'css)
+(ublt/set-up 'o-blog
+  (dolist (project
+           '(("blog"
+              :base-directory "~/Programming/projects/blog/src/"
+              :publishing-directory "~/Programming/projects/blog/public")
+             ("o-blog"
+              :base-directory "~/.emacs.d/lib/o-blog/example/"
+              :publishing-directory "~/.emacs.d/lib/o-blog/example/out")))
+    (add-to-list 'org-publish-project-alist project)))
 
 (ublt/set-up 'org-compat)
 (ublt/set-up 'ob-exp)
 (ublt/set-up 'ox)
+(ublt/set-up 'ob-sql)
+(ublt/set-up 'ob-scheme)
 
 (provide 'ublt-organization)
