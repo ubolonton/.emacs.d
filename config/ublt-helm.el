a584c1954e449f4d9030d8393a3d5400435ad439
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-11-29 15:29:05 +0700

    clean up helm config

--- config/ublt-helm.el
+++ config/ublt-helm.el
@@ -4,24 +4,34 @@
 
 ;;; TODO: Clean up
 
+;;; FIX: Check why helm flickers when input changes
+
 (require 'helm-config)
-(require 'helm-regexp)
-(ublt/set-up 'helm-files
-  (setq helm-ff-file-name-history-use-recentf t
-        helm-ff-search-library-in-sexp t)
-  (dolist (pattern '("\\.pyc$" "\\.elc$"))
-    (add-to-list 'helm-boring-file-regexp-list pattern)))
+
 (ublt/set-up 'helm-match-plugin
   (setq helm-mp-highlight-delay 0.7
         helm-mp-highlight-threshold 4))
+
+(ublt/set-up 'helm-files
+  (setq helm-ff-file-name-history-use-recentf t
+        helm-ff-auto-update-initial-value t
+        helm-ff-search-library-in-sexp t)
+  (dolist (pattern '("\\.pyc$" "\\.elc$"))
+    (add-to-list 'helm-boring-file-regexp-list pattern)))
 (ublt/set-up 'helm-buffers
   (setq helm-buffers-fuzzy-matching t))
-(ublt/set-up 'helm-net
-  (setq helm-google-suggest-use-curl-p (when (executable-find "curl") t)))
-(ublt/in '(gnu/linux)
-  ;;; Quote the the search string
-  (ublt/set-up 'helm-locate
+(ublt/set-up 'helm-locate
+  (ublt/in '(gnu/linux)
     (setq helm-locate-command "locate %s -e -A --regex %s")))
+
+(ublt/set-up 'helm-net
+  (setq helm-google-suggest-use-curl-p (when (executable-find "curl") t)
+        helm-home-url "https://www.google.com"))
+
+(ublt/set-up 'helm-regexp)
+(ublt/set-up 'helm-swoop)
+(ublt/set-up 'helm-cmd-t)
+
 (setq helm-maybe-use-default-as-input t
       helm-quick-update t
       helm-split-window-in-side-p 'below
@@ -35,32 +45,34 @@
   (let ((base '( ;; helm-c-source-ffap-line
                 ;; helm-c-source-ffap-guesser
                 helm-source-buffers-list
-                helm-source-ido-virtual-buffers
-                ;; helm-source-files-in-current-dir ; use ido
-                helm-source-pp-bookmarks
                 helm-source-recentf
+                helm-source-ido-virtual-buffers
+                helm-source-buffer-not-found
+                helm-source-files-in-current-dir
+                helm-source-pp-bookmarks
                 helm-source-file-cache
-                helm-source-locate
-                ;; helm-c-source-cmd-t-caches
-                ;; Additions
-                ;; helm-c-source-semantic
-                ;; helm-c-source-git-project-files
-                ;; helm-c-source-emacs-process
-                )))
+                helm-source-locate)))
     (if (featurep 'helm-cmd-t)
         ;; FIX
         (condition-case nil
-            (cons (helm-cmd-t-get-create-source (helm-cmd-t-root-data)) base)
+            (append base (list (helm-cmd-t-get-create-source (helm-cmd-t-root-data))))
           (error base))
       base)))
 
-(ublt/set-up 'helm-cmd-t)
+;;; FIX: Check why there is flickering with helm-occur and
+;;; helm-source-buffers-list but not helm-swoop while moving through
+;;; the result list. If it can be fixed, enable follow-mode for helm-source-buffers-list
+(defun ublt/helm-enable-follow-mode ()
+  (dolist (source (list helm-source-occur
+                        helm-source-moccur
+                        helm-source-org-headline))
+    (helm-attrset 'follow 1 source)))
+(ublt/helm-enable-follow-mode)
+;; (remove-hook 'helm-before-initialize-hook #'ublt/helm-enable-follow-mode)
 
 (defun ublt/helm ()
   (interactive)
   (helm-other-buffer (ublt/helm-sources) "*ublt/helm*"))
-
-;; TODO: Turn on follow-mode by default for helm-occur
 
 ;;; TODO: Maybe customize faces is better (per-source selection of
 ;;; fixed-pitch/variable-pitched font)?
@@ -69,26 +81,26 @@
   "Determine whether all of SOURCES should use variable-pitch
 font (fixed-pitch is still preferable)."
   (every (lambda (x)
-             (member x '(;; helm-c-source-ffap-line
-                         ;; helm-c-source-ffap-guesser
-                         ;; helm-c-source-buffers-list
-                         helm-source-bookmarks
-                         helm-source-pp-bookmarks
-                         ;; helm-source-recentf
-                         ;; helm-source-file-cache
-                         ;; helm-source-filelist
-                         ;; helm-source-files-in-current-dir+
-                         ;; helm-source-files-in-all-dired
-                         ;; helm-source-locate
-                         helm-source-emacs-process
-                         helm-source-org-headline
-                         helm-source-emms-streams
-                         helm-source-emms-files
-                         helm-source-emms-dired
-                         ;; helm-source-google-suggest
-                         helm-source-apt
-                         ;; helm-source-helm-commands
-                         )))
+           (member x '(;; helm-c-source-ffap-line
+                       ;; helm-c-source-ffap-guesser
+                       ;; helm-c-source-buffers-list
+                       helm-source-bookmarks
+                       helm-source-pp-bookmarks
+                       ;; helm-source-recentf
+                       ;; helm-source-file-cache
+                       ;; helm-source-filelist
+                       ;; helm-source-files-in-current-dir+
+                       ;; helm-source-files-in-all-dired
+                       ;; helm-source-locate
+                       helm-source-emacs-process
+                       helm-source-org-headline
+                       helm-source-emms-streams
+                       helm-source-emms-files
+                       helm-source-emms-dired
+                       ;; helm-source-google-suggest
+                       helm-source-apt
+                       ;; helm-source-helm-commands
+                       )))
          sources))
 (defun ublt/helm-tweak-appearance ()
   "Use `variable-pitch' font for helm if it's suitable for
@@ -97,7 +109,8 @@ all of the sources."
     (when (ublt/helm-should-use-variable-pitch? helm-sources)
       (variable-pitch-mode +1))
     (setq line-spacing 0.6)
-    (text-scale-increase 1)))
+    ;; (text-scale-increase 1)
+    ))
 (add-hook 'helm-after-initialize-hook 'ublt/helm-tweak-appearance)
 ;;; XXX: Big hack!
 ;;; TODO: Move to ublt-appearance?
@@ -118,8 +131,6 @@ all of the sources."
 ;;     (other-window 1)
 ;;     (call-interactively 'helm-exit-minibuffer))
 ;;   )
-
-(ublt/set-up 'helm-swoop)
 
 (helm-mode +1)
 
