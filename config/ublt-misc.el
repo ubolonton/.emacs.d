741a17802fb4176ffc3a7099dec553c23fc96f6d
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2015-02-17 20:20:43 +0700

    make hippie-expand more usable (copied from starterkit)

--- config/ublt-misc.el
+++ config/ublt-misc.el
@@ -178,13 +178,13 @@
 ;;; Seed the random-number generator.
 (random t)
 
-;; (eval-after-load 'hippie-exp
-;;   '(progn
-;;      (dolist (f '(try-expand-line try-expand-list try-complete-file-name-partially))
-;;        (delete f hippie-expand-try-functions-list))
+(eval-after-load 'hippie-exp
+  '(progn
+     (dolist (f '(try-expand-line try-expand-list try-complete-file-name-partially))
+       (delete f hippie-expand-try-functions-list))
 
-;;      ;; Add this back in at the end of the list.
-;;      (add-to-list 'hippie-expand-try-functions-list 'try-complete-file-name-partially t)))
+     ;; Add this back in at the end of the list.
+     (add-to-list 'hippie-expand-try-functions-list 'try-complete-file-name-partially t)))
 
 ;; (eval-after-load 'grep
 ;;   '(when (boundp 'grep-find-ignored-files)
