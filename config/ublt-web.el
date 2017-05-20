f0ad976ee3531337930c05dc7709c2abc3300524
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2017-05-20 11:15:59 +0700

    use web-mode to open .tsx files

--- config/ublt-web.el
+++ config/ublt-web.el
@@ -9,8 +9,9 @@
   (add-hook 'web-mode-hook (ublt/off-fn 'auto-fill-mode))
 
   ;; Use `web-mode', as `jsx-mode' is actually not ReactJS's..
-  ;; FIX: This is a bad hack
   (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tsx$" . web-mode))
+  ;; FIX: This is a bad hack
   (defadvice web-mode-highlight-part (around tweak-jsx activate)
     (if (equal web-mode-content-type "jsx")
         (let ((web-mode-enable-part-face nil))
