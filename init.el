faa54a4b58809d1ba97a81072ca0e0566138a388
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2019-05-07 13:42:16 +0700

    Don't download straight.el, as we already have it as a submodule

--- init.el
+++ init.el
@@ -56,13 +56,6 @@
      (let ((bootstrap-file
             (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
            (bootstrap-version 5))
-       (unless (file-exists-p bootstrap-file)
-         (with-current-buffer
-             (url-retrieve-synchronously
-              "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-              'silent 'inhibit-cookies)
-           (goto-char (point-max))
-           (eval-print-last-sexp)))
        (load bootstrap-file nil 'nomessage))
 
      ;; TODO: Remove this once https://github.com/raxod502/straight.el/issues/377 is fixed.
