c323492c34e0d944ac4f2468b9ac3afc6fa36b22
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2019-08-11 21:37:37 +0700

    Use emacs-tree-sitter

--- config/ublt-misc.el
+++ config/ublt-misc.el
@@ -114,6 +114,11 @@
 ;; Who the hell set it to t?
 (setq debug-on-error nil)
 
+(use-package tree-sitter
+  :ensure nil :straight nil
+  :demand t
+  :load-path "~/Programming/projects/emacs-tree-sitter/")
+
 
 ;;; Misc stuff I use -------------------------------------------------
 
