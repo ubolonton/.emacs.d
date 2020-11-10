54df292cd3b76992b5e337dc3e7b296d66a94b93
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2020-11-10 12:49:10 +0700

    Patch tsc and tree-sitter-langs recipes for local dev

--- lib/ublt-straight-recipes.el
+++ lib/ublt-straight-recipes.el
@@ -21,20 +21,32 @@ base recipe, call this function without specifying PLIST.
 
 Example:
 
-    (straight-transaction
-      (ublt/straight-override 'hydra
-        :fork (ublt/-fork \"hydra\" :branch \"delayed-hiding\"))
-      (ublt/straight-override 'lv
-        :fork (ublt/-fork \"hydra\" :branch \"delayed-hiding\")))
+    (ublt/straight-override 'hydra
+      :fork (ublt/-fork \"hydra\" :branch \"delayed-hiding\"))
+    (ublt/straight-override 'lv
+      :fork (ublt/-fork \"hydra\" :branch \"delayed-hiding\"))
 "
   (declare (indent 1))
   (straight-register-package (apply #'ublt/straight-recipe package plist)))
+
+(defun ublt/straight-add-files (package files)
+  "Add more FILES to the recipe for PACKAGE."
+  (declare (indent 1))
+  (ublt/straight-override package
+    :files (append (plist-get (cdr (ublt/straight-recipe package)) :files)
+                   files)))
 
 (cl-defun ublt/-fork (name &key branch)
   `(:repo ,(format "github:ubolonton/%s" name)
           :host nil ; XXX: Otherwise straight tries to construct https URL.
           :remote "ubolonton"
           :branch ,branch))
+
+(ublt/straight-add-files 'tsc
+  '("core/DYN-VERSION" "core/tsc-dyn.*"))
+
+(ublt/straight-add-files 'tree-sitter-langs
+  '("langs/bin"))
 
 (straight-register-package
  '(wat-mode :type git :host github :repo "devonsparks/wat-mode"))
