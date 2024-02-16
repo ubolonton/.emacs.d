7c2abc64ee97fbb3f3e14569747f363bf9145a65
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2024-02-16 10:43:38 +0700

    lisp: Update `clj-refactor` to 3.11.3

--- straight/versions/default.el
+++ straight/versions/default.el
@@ -12,7 +12,7 @@
  ("cargo.el" . "1f3bc0d10dbddabfe1974590847a1359aeb3c244")
  ("cask-mode" . "7c6719d3bb4fe552958634bd5a11abc56681f3a7")
  ("cider" . "06dc45844ad05fe23fccb7dec1609404e3cda88e")
- ("clj-refactor.el" . "fd58583b4a27ed83e2bf9e4192f755003291d8ef")
+ ("clj-refactor.el" . "fa3efe18e7150df5153a7d05c54e96d59398a0a8")
  ("clojure-mode" . "25d713a67d8e0209ee74bfc0153fdf677697b43f")
  ("company-mode" . "955f80e6c911a818611887bc35658af9e925ed66")
  ("company-posframe" . "37a1f136f61bed4050dbeb182767c7e51cb73e1b")
